4a25cf394a4d798a085b82c576d81bc1ebf82839
XxxUrviShah <124638258+XxxUrviShah@users.noreply.github.com>
2023-02-15 21:06:00 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+differnce=0
+rightWristX=0
+leftWristX=0
+
+function setup(){
+  video=createCapture=(VIDEO);
+  video.size(550,500);
+  
+  canvas = createCanvas(550,500);
+  cancas.postion(560,150);
+  posenet=ml5.posnet(viseo,modeload);
+  posenet.on('pose',gotPoses);
+}
+
+function modeloaded(){
+  if(results.length > 0)
+{
+console.log(results);
+  leftWristX =results[0].pose.leftWrist.x;
+  rightWristX = results[0].pose.lesftWrist.x;
+  difference = floor(leftWristX - rightWrist);
+  
+  console.log("leftWrisX = " + leftWristX + "rightWristX= " + rightWristX);
+} 
+}
+function draw() {
+background('#6C91C2');
+
+  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+textSize(difference);
+fill('#FFE787');
+text('Urvi', 50, 400);
+}
